f2156bb1bae5cad402378f33305e0d762d8c9b89
Yuce Ozan <yuce.ozan1000@gmail.com>
2022-05-31 02:50:42 +0300

    Created CSV draft + Start information scrap code within the links

--- projet.py
+++ projet.py
@@ -3,20 +3,44 @@ from bs4 import BeautifulSoup
 
 links = []
 
-for i in range(4):
-    print(i)
-
 url = "http://books.toscrape.com/catalogue/category/books/fantasy_19/index.html"
 page = requests.get(url)
 if page.ok:
 
     soup = BeautifulSoup(page.content, 'html.parser')
-    products = soup.findAll("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
+    products = soup.findAll("article", class_="product_pod")
+    tables = soup.findAll('table', {'class': 'table table-striped'})
+"""
+    for product in products:
+        a = product.find('a')
+        link = a['href'].replace("../../..", "")
+        links.append("http://books.toscrape.com/catalogue" + link)
+
+url = "http://books.toscrape.com/catalogue/category/books/fantasy_19/page-2.html"
+page = requests.get(url)
+if page.ok:
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    products = soup.findAll("article", class_="product_pod")
 
     for product in products:
         a = product.find('a')
-        link = a['href']
-        linkFull = link.replace("../../..", "")
-        links.append("http://books.toscrape.com/catalogue" + linkFull)
+        link = a['href'].replace("../../..", "")
+        links.append("http://books.toscrape.com/catalogue" + link)
+        print(links)
+        
+with open('urls.csv', 'w') as file:
+    for link in links:
+        file.write(link + '\n')
 
-    print(links)
+with open('urls.csv', 'r') as file:
+    for row in file:
+        print(row)
+"""
+
+url = 'http://books.toscrape.com/catalogue/every-heart-a-doorway-every-heart-a-doorway-1_465/index.html'
+
+response = requests.get(url)
+if response.ok:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    UPC = soup.find('table', {'class': 'table table-striped'}).find('td', {})
