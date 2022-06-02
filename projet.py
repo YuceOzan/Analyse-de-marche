1c511358777507fdb651f92453c24b92663f606f
Yuce Ozan <yuce.ozan1000@gmail.com>
2022-06-02 19:12:27 +0300

    Starting create loop for categories from 'Books to Scrape' main page

--- projet.py
+++ projet.py
@@ -1,16 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = 'http://books.toscrape.com'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "html.parser")
+
+books = []
+
 links = []
 
-url = "http://books.toscrape.com/catalogue/category/books/fantasy_19/index.html"
-page = requests.get(url)
-if page.ok:
-    soup = BeautifulSoup(page.content, 'html.parser')
-    products = soup.findAll("article", class_="product_pod")
-    tables = soup.findAll('table', {'class': 'table table-striped'})
+categories = soup.findAll("ul", class_="nav nav-list")
+
+for category in categories:
+    category_link = category.find('li').a.get('href')
+    link_base = "http://books.toscrape.com/"
+    full_link = link_base + category_link
+    print(full_link)
+
 """
-    for product in products:
+
+for product in products:
         a = product.find('a')
         link = a['href'].replace("../../..", "")
         links.append("http://books.toscrape.com/catalogue" + link)
@@ -31,24 +40,23 @@ with open('urls.csv', 'w') as file:
 with open('urls.csv', 'r') as file:
     for row in file:
         print(row)
-"""
+
 
 url = 'http://books.toscrape.com/catalogue/unicorn-tracks_951/index.html'
-
 response = requests.get(url)
-if response.ok:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    upc = soup.find('table', {'class': 'table table-striped'}).find_all('td')[0].text
-    title = soup.find('div', {'class', 'col-sm-6 product_main'}).find('h1').text
-    price_with_tax = soup.find('table', {'class': 'table table-striped'}).find_all('td')[3].text.replace('Â', '')
-    pricetax = soup.find('table', {'class': 'table table-striped'}).find_all('td')[2].text.replace('Â', '')
-    available = soup.find('table', {'class': 'table table-striped'}).find_all('td')[5].text
-    category = soup.find('ul', {'class', 'breadcrumb'}).find_all('a')[2].text
-    review = soup.find('table', {'class', 'table table-striped'}).find('td').text[-1]
-    image = soup.find('div', {'class', 'item active'}).find('img')['src']
-##Concatener url image avec 'append'
-    description = soup.select("article.product_page > p")
-    #description = soup.find('article', {'class': 'product_page'}).find().text
+soup = BeautifulSoup(response.content, 'html.parser')
 
 
-    print(image)
+livre = {
+        'upc': soup.find('table', {'class': 'table table-striped'}).find_all('td')[0].text,
+        'title': soup.find('div', {'class', 'col-sm-6 product_main'}).find('h1').text,
+        'price_with_tax': soup.find('table', {'class': 'table table-striped'}).find_all('td')[3].text.replace('Â', ''),
+        'pricetax': soup.find('table', {'class': 'table table-striped'}).find_all('td')[2].text.replace('Â', ''),
+        'available': soup.find('table', {'class': 'table table-striped'}).find_all('td')[5].text,
+        'category': soup.find('ul', {'class', 'breadcrumb'}).find_all('a')[2].text,
+        'review': soup.find('table', {'class', 'table table-striped'}).find('td').text[-1],
+        'image': soup.find('div', {'class', 'item active'}).find('img')['src'].replace('../..', 'http://books.toscrape.com'),
+        'description': soup.select("article.product_page > p"),
+    }
+print(livre)
+"""
