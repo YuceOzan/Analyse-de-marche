9aefe5f37b80fa5731246307f580d1d29281cd87
Yuce Ozan <yuce.ozan1000@gmail.com>
2022-06-20 12:49:27 +0300

    creating One CSV file to contain all informations. Need to modify in order to have several csv file for each category.

--- projet.py
+++ projet.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import csv
 url = 'http://books.toscrape.com'
 
 
@@ -27,61 +27,69 @@ def get_links(new_categories):
     for link in new_categories:
         r2 = requests.get(link).text
         book_soup = BeautifulSoup(r2, "html.parser")
-        # print("categorie: " + link)
+        print("categorie: " + link)
         nextpage = True
         while nextpage:
             book_link = book_soup.find_all(class_="product_pod")
             for product in book_link:
                 a = product.find('a')
                 full_link = a['href'].replace("../../..", "http://books.toscrape.com/catalogue")
-                # print("livre: " + full_link)
+                print("livre: " + full_link)
                 books.append(full_link)
             if book_soup.find('li', class_='next') is None:
                 nextpage = False
-                # print("pas de page suivante")
+                print("pas de page suivante")
             else:
                 next_page = book_soup.select_one('li.next>a').get('href')
                 index = link.replace('/index.html', '')
                 pg = next_page
                 real_url = f"{index}/{pg}"
-                # print("Page suivante: " + real_url)
+                print("Page suivante: " + real_url)
                 r4 = requests.get(real_url).text
                 book_soup = BeautifulSoup(r4, 'html.parser')
-        fp = open('urls.csv', 'w')
-        for book_url in books:
-            r4 = requests.get(book_url).text
-            book_url_soup = BeautifulSoup(r4, 'html.parser')
-            book = get_book(book_url_soup)
-            fp.write(book["upc"])
-        fp.close()
+        with open("test.csv", 'w', newline='', encoding="utf-8") as csvfile:
+            for book_url in books:
+                r4 = requests.get(book_url).text
+                book_url_soup = BeautifulSoup(r4, 'html.parser')
+                book = get_book(book_url_soup)
+
+                fieldnames = ["upc", "title", "price_including_taxes", 'price_excluding_tax', 'product_description', 'price_including_tax', 'number_available', 'image_url', 'category', 'review-rating']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writerow(book)
+
+
 
     return books
 
 
-
-
-
 def get_book(book_url_soup):
-
     upc = book_url_soup.find(class_="table table-striped").td.text
+    # print(upc)
     title = book_url_soup.find(class_="col-sm-6 product_main").h1.text
+    # print(title)
     price_with_tax = book_url_soup.find(class_="table table-striped").find_all('td')[3].text.replace('Â£', '')
+    # print(price_with_tax)
     price_without_tax = book_url_soup.find(class_="table table-striped").find_all('td')[2].text.replace('Â£', '')
+    # print(price_without_tax)
     available = book_url_soup.find(class_="table table-striped").find_all('td')[5].text.replace('In stock (',
                                                                                                 '').replace(')', '')
+    # print(available)
     book_category = book_url_soup.find(class_="breadcrumb").find_all('a')[2].text
+    # print(book_category)
     image = book_url_soup.find(class_="item active").find('img')['src'].replace("../..", "http://books.toscrape.com")
+    # print(image)
+    image_download = requests.get(image).content
     description = book_url_soup.find(class_="product_page").find_all('p')[3].text.replace("â", '"') \
         .replace("â", '"').replace("â", "'").replace("â", "—").replace("â¢Â", "•").replace("Â", " ").replace(
-        "â¢", "•")
+        "â¢", "•").replace('â¦', "…").replace("Ã¢â¬", "").replace("Ã¢â¬", "").replace("Ã¢", "").replace("â", "–")
+    # print(description)
     review = book_url_soup.find('p', class_='star-rating').get('class')[-1]
-    print(review)
+    # print(review)
 
-    book = {"title": title, "upc": upc, "price_including_tax(£)": price_with_tax,
-            "price_excluding_tax(£)": price_without_tax,
+    book = {"title": title, "upc": upc, "price_including_tax": price_with_tax,
+            "price_excluding_tax": price_without_tax,
             "number_available": available, "product_description": description, "category": book_category,
             "image_url": image, "review-rating": review}
-
 
     return book
 
@@ -91,5 +99,4 @@ project = []
 s = page(url)
 l = get_categorie(s)
 m = get_links(l)
-z = get_book(m)
-
+o = get_book(m)
